357aaa4c9dac53f2f4b2c65c9203a5c9975d84c7
Tylan Coleman <58373062+ColemanTylan@users.noreply.github.com>
2024-08-16 23:17:02 -0400

    defect searching wrote comments shouldnt be much longer, took time off earlier today

--- Solution.java
+++ Solution.java
@@ -9,13 +9,13 @@ import java.util.*;
 }
 
 class FamilyMember {
-
+    //Creating Senior Member method
     public void seniorMember(int budget, int moneySpend) {
         System.out.println("Senior Member");
         System.out.println("Spend: " + moneySpend);
         System.out.println("Budget Left: " + (budget - moneySpend));
     }
-
+    //Creating Junior Member method
     public void juniorUser(int budget, int moneySpend) {
         System.out.println("Junior Member");
         System.out.println("Spend: " + moneySpend);
@@ -25,8 +25,11 @@ class FamilyMember {
 
 public class Solution {
     public static void main(String[] args) {
+        //Storing Input Stream for method use on 'in' variable
         Scanner in = new Scanner(System.in);
+        //Turns the input collected into integers and prompts end user input via terminal
         int testCases = Integer.parseInt(in.nextLine());
+        // While input collected (integers) more than 0 run method
         while (testCases > 0) {
             String role = in.next();
             int spend = in.nextInt();
